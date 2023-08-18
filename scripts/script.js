928f607c5e8aef4cf4e668fd6d7453ffce311dc2
Jindy01 <vip.fsafds@mail.ru>
2023-08-18 19:57:19 +0300

    Point

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,23 @@
-'use strict';
+"use strict";
+
+console.log('Подключено');
 
 function closed() {
-    alert('В разработке');
+  alert("В разработке");
+}
+
+function openPage(pageName) {
+  let i, tabcontent, buttons;
+
+  for (i = 0; i < tabcontent.length; ++i) {
+    tabcontent[i].style.display = "none";
+  }
+
+  buttons = document.getElementsByClassName("buttons");
+  for (i = 0; i < buttons.length; ++i) {
+    buttons[i].className = buttons[i].className.replace("active", "");
+  }
+
+  document.getElementById(pageName).style.display = "block";
+  alert('оТРАБОТАНО')
 }
