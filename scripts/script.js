614a998fcc89b3a228ac1acd2801a6185a728cf3
Jindy01 <vip.fsafds@mail.ru>
2023-08-19 22:29:46 +0300

    New Script And HTML

--- scripts/script.js
+++ scripts/script.js
@@ -6,18 +6,40 @@ function closed() {
   alert("В разработке");
 }
 
+//  Для перехода между страничками
 function openPage(pageName) {
-  let i, tabcontent, buttons;
+  let i, tabcontent, tabcontentAsort, a;
 
+  tabcontent = document.getElementsByClassName('scroll')
   for (i = 0; i < tabcontent.length; ++i) {
     tabcontent[i].style.display = "none";
   }
 
-  buttons = document.getElementsByClassName("buttons");
-  for (i = 0; i < buttons.length; ++i) {
-    buttons[i].className = buttons[i].className.replace("active", "");
+  tabcontentAsort = document.getElementsByClassName('scroll_asortiment')
+  for (i = 0; i < tabcontentAsort.length; ++i) {
+    tabcontentAsort[i].style.display = "none";
   }
 
-  document.getElementById(pageName).style.display = "block";
-  alert('оТРАБОТАНО')
+  a = document.getElementsByClassName("a");
+  for (i = 0; i < a.length; ++i) {
+    a[i].className = a[i].className.replace("active", "");
+  }
+
+  document.getElementById(pageName).style.display = "flex";
+}
+
+function openPage(pageName) {
+  let i, tabcontent, tabcontentAsort, a;
+
+  tabcontent = document.getElementsByClassName('scroll')
+  for (i = 0; i < tabcontent.length; ++i) {
+    tabcontent[i].style.display = "none";
+  }
+
+  a = document.getElementsByClassName("a");
+  for (i = 0; i < a.length; ++i) {
+    a[i].className = a[i].className.replace("active", "");
+  }
+
+  document.getElementById(pageName).style.display = "flex";
 }
